e1943cc2d9ec34b82235bf48a7c326aa1cedc09f
Plamen Georgiev Petkov <plamen.georgiev@iecisa.com>
2020-03-12 18:30:23 +0100

    usado xwork para el enlace de confluencer

--- src/main/java/com/cis/confluence/plugins/impl/ConfluencerParticipate.java
+++ src/main/java/com/cis/confluence/plugins/impl/ConfluencerParticipate.java
@@ -1,25 +1,81 @@
-//package com.cis.confluence.plugins.impl;
-//
-//
-//import com.atlassian.confluence.renderer.radeox.macros.MacroUtils;
-//import com.atlassian.confluence.security.Permission;
-//import com.atlassian.confluence.security.PermissionManager;
-//import com.atlassian.confluence.setup.settings.SettingsManager;
-//import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
-//import com.atlassian.confluence.user.UserAccessor;
-//import com.atlassian.confluence.user.UserDetailsManager;
-//import com.atlassian.confluence.userstatus.StatusTextRenderer;
-//import com.atlassian.confluence.util.GeneralUtil;
-//import com.atlassian.confluence.util.velocity.VelocityUtils;
-//import com.atlassian.renderer.RenderContext;
-//import com.atlassian.renderer.v2.RenderMode;
-//import com.atlassian.renderer.v2.macro.BaseMacro;
-//import com.atlassian.renderer.v2.macro.MacroException;
-//import com.atlassian.user.User;
-//import java.util.Arrays;
-//import java.util.Map;
-//import org.apache.commons.lang3.StringUtils;
-//
-//public class ConfluencerParticipate extends Abstractwe {
-//
-//}
+package com.cis.confluence.plugins.impl;
+
+
+import com.atlassian.confluence.core.ConfluenceActionSupport;
+import com.atlassian.confluence.labels.Label;
+import com.atlassian.confluence.labels.LabelManager;
+import com.atlassian.confluence.labels.Labelable;
+import com.atlassian.confluence.pages.AbstractPage;
+import com.atlassian.confluence.pages.actions.PageAware;
+import com.atlassian.confluence.renderer.radeox.macros.MacroUtils;
+import com.atlassian.confluence.security.Permission;
+import com.atlassian.confluence.security.PermissionManager;
+import com.atlassian.confluence.setup.settings.SettingsManager;
+import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
+import com.atlassian.confluence.user.UserAccessor;
+import com.atlassian.confluence.user.UserDetailsManager;
+import com.atlassian.confluence.userstatus.StatusTextRenderer;
+import com.atlassian.confluence.util.GeneralUtil;
+import com.atlassian.confluence.util.velocity.VelocityUtils;
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.RenderMode;
+import com.atlassian.renderer.v2.macro.BaseMacro;
+import com.atlassian.renderer.v2.macro.MacroException;
+import com.atlassian.user.User;
+import java.util.Arrays;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+
+public class ConfluencerParticipate extends ConfluenceActionSupport implements PageAware {
+
+    private AbstractPage page;
+    private boolean participa;
+
+    public ConfluencerParticipate() {
+
+        this.participa = false;
+    }
+
+    @Override
+    public String doDefault() throws Exception {
+        return "ERROR";
+    }
+
+    @Override
+    public String execute() throws Exception {
+        return "SUCCESS";
+    }
+
+    public boolean isParticipa() {
+        return participa;
+    }
+
+    public void setParticipa(boolean participa) {
+        this.participa = participa;
+    }
+
+    @Override
+    public AbstractPage getPage() {
+        return page;
+    }
+
+    @Override
+    public void setPage(AbstractPage abstractPage) {
+        this.page = abstractPage;
+    }
+
+    @Override
+    public boolean isPageRequired() {
+        return false;
+    }
+
+    @Override
+    public boolean isLatestVersionRequired() {
+        return false;
+    }
+
+    @Override
+    public boolean isViewPermissionRequired() {
+        return false;
+    }
+}
